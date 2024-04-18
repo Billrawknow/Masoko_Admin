29867cbcc021c4c7e2f377e8b447f6a8c514f113
Bildad Ronoh <Bildadronoh@gmail.com>
2024-04-18 12:54:20 +0300

    make the listproducts responsive

--- src/Components/ListProduct/ListProduct.jsx
+++ src/Components/ListProduct/ListProduct.jsx
@@ -48,7 +48,7 @@ const ListProduct = () => {
               <p>${product.old_price} </p>
               <p>${product.new_price} </p>
               <p>{product.category}</p>
-              <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt="" />
+              <img onClick={()=>{remove_product(product.id)}} className='list-product-remove-icon' src={cross_icon} alt="" />
           </div>
           <hr />
           </> 
